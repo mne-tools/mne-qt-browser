0a1cf6e3a51e18077f0677260207f27e4f0e4f1e
marsipu <dev@earthman-music.de>
2021-09-01 12:57:59 +0200

    setup repo

---
+++ mne_pg_browser/__main__.py
@@ -0,0 +1,25 @@
+import mne
+import os
+import numpy as np
+
+from mne.viz._figure import use_browser_backend
+
+
+def main():
+    sample_data_folder = mne.datasets.sample.data_path()
+    sample_data_raw_file = os.path.join(sample_data_folder, 'MEG', 'sample',
+                                        'sample_audvis_raw.fif')
+    raw = mne.io.read_raw(sample_data_raw_file)
+
+    with use_browser_backend('pyqtgraph'):
+        onsets = np.arange(2, 8, 2) + raw.first_time
+        durations = np.repeat(1, len(onsets))
+        descriptions = ['Test1', 'Test2', 'Test3']
+        for onset, duration, description in zip(onsets, durations, descriptions):
+            raw.annotations.append(onset, duration, description)
+
+        raw.plot(block=True)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(name='mne_pg_browser',
+      version='0.0.1',
+      author='marsipu',
+      author_email='dev@earthman-music.de',
+      description='A new backend based on pyqtgraph for the 2D-Data-Browser in MNE-Python.',
+      license='License :: OSI Approved :: BSD License',
+      url='https://github.com/marsipu/mne_pg_browser',
+      project_urls={'Bug Tracker': 'https://github.com/marsipu/mne_pg_browser/issues'},
+      classifiers=['Programming Language :: Python :: 3',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System:: OS Independent'],
+      packages=find_packages(),
+      install_requires=['numpy',
+                        'scipy',
+                        'matplotlib',
+                        'PyQt5',
+                        'qtpy',
+                        'mne',
+                        'pyqtgraph @ git+https://git@github.com/pyqtgraph/pyqtgraph@master#egg=pyqtgraph',
+                        'pyopengl',
+                        'pytest-qt'],
+      entrypoints={'console_scripts': ['mne_pg_browser = mne_pg_browser.__main__:main']}
+      )
