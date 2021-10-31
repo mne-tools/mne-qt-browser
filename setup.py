d3f1fa1ce49c4e77fdf8b5cd5d7a31a43b11ca2a
marsipu <dev@earthman-music.de>
2021-10-31 21:22:27 +0100

    MNT: make ready for PyPi upload

--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
+import pathlib
+
 from setuptools import setup, find_packages
 
-setup(name='mne_qt_browser',
-      version='0.0.1',
-      author='marsipu',
-      author_email='dev@earthman-music.de',
+readme = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setup(name='mne-qt-browser',
+      version='0.1.0',
+      maintainer='Martin Schulz',
+      maintainer_email='dev@earthman-music.de',
       description='A new backend based on pyqtgraph for the 2D-Data-Browser '
                   'in MNE-Python.',
+      long_description=readme,
+      long_description_content_type='text/markdown',
       license='License :: OSI Approved :: BSD License',
-      url='https://github.com/marsipu/mne_pg_browser',
+      url='https://github.com/mne-tools/mne-qt-browser',
       project_urls={'Bug Tracker':
-                    'https://github.com/marsipu/mne_pg_browser/issues'},
+                    'https://github.com/mne-tools/mne-qt-browser/issues'},
       classifiers=['Programming Language :: Python :: 3',
                    'License :: OSI Approved :: BSD License',
-                   'Operating System:: OS Independent'],
+                   'Operating System :: OS Independent'],
       packages=find_packages(),
       install_requires=['numpy',
                         'scipy',
