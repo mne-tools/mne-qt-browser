ffd5315af197c521f7fa49e40bfd03e11bb9c442
marsipu <dev@earthman-music.de>
2021-10-31 16:45:05 +0100

    ADD: Add speed test

--- mne_qt_browser/conftest.py
+++ mne_qt_browser/conftest.py
@@ -4,8 +4,8 @@ from mne.viz import use_browser_backend
 from mne.conftest import *  # noqa: F401, F403
 
 
-@pytest.fixture(params=['pyqtgraph'])
-def browse_backend(request, garbage_collect):
+@pytest.fixture
+def browser_backend(garbage_collect):
     """Parametrizes the name of the browser backend."""
-    with use_browser_backend(request.param) as backend:
+    with use_browser_backend('pyqtgraph') as backend:
         yield backend
