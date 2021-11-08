870611dc28e2e638109706849122637c18e8904b
marsipu <dev@earthman-music.de>
2021-11-08 21:42:44 +0100

    MNT: Remove redundant conftest.py

--- mne_qt_browser/conftest.py
+++ mne_qt_browser/conftest.py
@@ -6,7 +6,18 @@
 
 import pytest
 
+from mne.viz import use_browser_backend
+from mne.conftest import *  # noqa: F401, F403
+
+
 _store = dict()
+
+
+@pytest.fixture
+def browser_backend(garbage_collect):
+    """Parametrizes the name of the browser backend."""
+    with use_browser_backend('pyqtgraph') as backend:
+        yield backend
 
 
 def pytest_configure(config):
