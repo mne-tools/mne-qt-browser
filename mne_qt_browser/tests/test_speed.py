8f4bebca8f27f1348b9cf7f3f9bc804af5b7d31e
marsipu <dev@earthman-music.de>
2022-02-16 15:21:38 +0100

    fix version-comparison vor 1.0.dev0 not working

--- mne_qt_browser/tests/test_speed.py
+++ mne_qt_browser/tests/test_speed.py
@@ -156,7 +156,7 @@ def test_scroll_speed_raw(raw_orig, benchmark_param, store,
 def _check_epochs_version():
     import mne
     from packaging.version import parse
-    if parse(mne.__version__) < parse('1.0'):
+    if parse(mne.__version__) < parse('1.0.'):
         pytest.skip('Epochs-Test were skipped because of mne < 1.0!')
 
 
