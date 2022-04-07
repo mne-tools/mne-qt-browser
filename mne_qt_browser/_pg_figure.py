03c58c995657c7197c063103a5bbd8126f5c5c70
Eric Larson <larson.eric.d@gmail.com>
2022-04-07 15:52:03 -0400

    FIX: Simpler

--- mne_qt_browser/_pg_figure.py
+++ mne_qt_browser/_pg_figure.py
@@ -184,7 +184,7 @@ def _safe_splash(meth):
             meth(self, *args, **kwargs)
         finally:
             try:
-                close = self.mne.splash.close()
+                self.mne.splash.close()
             except Exception:
                 pass
     return func
