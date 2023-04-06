ffa3681208cf38f82733d8f9a4e4277de8f3c8ca
Eric Larson <larson.eric.d@gmail.com>
2023-04-05 18:36:04 -0600

    FIX: Hierarchy

--- mne_qt_browser/__init__.py
+++ mne_qt_browser/__init__.py
@@ -1,4 +1,11 @@
-from ._version import __version__  # noqa
+try:
+    from importlib.metadata import version
+    __version__ = version("mne_qt_browser")
+except Exception:
+    try:
+        from ._version import __version__  # written by setuptools_scm
+    except ImportError:
+        __version__ = '0.0.0'
 
 # All created brower-instances are listed here for a reference to avoid having
 # them garbage-collected prematurely.
