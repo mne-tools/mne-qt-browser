1d1e20d0331ca1de049373d68684616d545c01b1
Eric Larson <larson.eric.d@gmail.com>
2023-07-31 13:12:11 -0400

    MAINT: Use pre-commit and updates for NumPy

--- mne_qt_browser/conftest.py
+++ mne_qt_browser/conftest.py
@@ -4,24 +4,23 @@
 #
 # License: BSD-3-Clause
 
-
+import os
 import pytest
 
-from mne.conftest import (raw_orig, pg_backend, garbage_collect)  # noqa: F401
+from mne.conftest import raw_orig, pg_backend, garbage_collect  # noqa: F401
 
-_store = {'Raw': {},
-          'Epochs_unicolor': {},
-          'Epochs_multicolor': {}}
+_store = {"Raw": {}, "Epochs_unicolor": {}, "Epochs_multicolor": {}}
 
 
 def pytest_configure(config):
     """Configure pytest options."""
     # Markers
-    for marker in ('benchmark', 'pgtest', 'slowtest'):
-        config.addinivalue_line('markers', marker)
+    for marker in ("benchmark", "pgtest", "slowtest"):
+        config.addinivalue_line("markers", marker)
+    os.environ["_MNE_BROWSER_BACK"] = "true"
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def store():
     """Yield our storage object."""
     yield _store
@@ -32,12 +31,13 @@ def pytest_terminal_summary(terminalreporter, exitstatus, config):
     if not any(len(_store[key]) for key in _store):
         return
     writer = terminalreporter
-    writer.line('')  # newline
-    writer.write_sep('=', 'benchmark results')
+    writer.line("")  # newline
+    writer.write_sep("=", "benchmark results")
     for type_name, results in _store.items():
-        writer.write_sep('-', type_name)
+        writer.write_sep("-", type_name)
         for name, vals in results.items():
             writer.line(
-                f'{name}:\n'
+                f"{name}:\n"
                 f'    Horizontal: {vals["h"]:6.2f}\n'
-                f'    Vertical:   {vals["v"]:6.2f}')
+                f'    Vertical:   {vals["v"]:6.2f}'
+            )
