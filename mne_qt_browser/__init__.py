a5528998696db985e2a5f1899de3b75549f1a166
Martin Schulz <dev@earthman-music.de>
2023-11-30 17:07:46 +0100

    Remove version file (#217)

--- mne_qt_browser/__init__.py
+++ mne_qt_browser/__init__.py
@@ -3,10 +3,7 @@ try:
 
     __version__ = version("mne_qt_browser")
 except Exception:
-    try:
-        from ._version import __version__  # written by setuptools_scm
-    except ImportError:
-        __version__ = "0.0.0"
+    __version__ = "0.0.0"
 
 # All created brower-instances are listed here for a reference to avoid having
 # them garbage-collected prematurely.
