d4babb773330671464cec75af7422ef6ec11ba8f
Clemens Brunner <clemens.brunner@gmail.com>
2024-03-15 10:13:43 +0100

    Fix line length

--- mne_qt_browser/_fixes.py
+++ mne_qt_browser/_fixes.py
@@ -14,8 +14,8 @@ try:
     from pytestqt.exceptions import capture_exceptions
 except ImportError:
     logger.debug(
-        "If pytest-qt is not installed, errors from inside the event loop will be occluded "
-        "and it will be harder to trace back the cause."
+        "If pytest-qt is not installed, errors from inside the event loop will be "
+        "occluded and it will be harder to trace back the cause."
     )
 
     @contextmanager
