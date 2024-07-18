f3d0f260d4a5ee341d624a58074b67d0febd1e71
NoahMarkowitz <noahb.markowitz@gmail.com>
2024-07-18 15:54:06 -0400

    update sensitivity unit test

--- mne_qt_browser/tests/test_pg_specific.py
+++ mne_qt_browser/tests/test_pg_specific.py
@@ -297,15 +297,15 @@ def test_pg_settings_dialog(raw_orig, pg_backend):
 
     ch_scale_spinbox = fig.mne.fig_settings.ch_scaling_spinboxes[ch_type_test]
     ch_sens_spinbox = fig.mne.fig_settings.ch_sensitivity_spinboxes[ch_type_test]
-    scaling_spinbox_value = round(ch_spinbox.value())
-    sensitivity_spinbox_value = round(ch_sens_spinbox.value())
+    scaling_spinbox_value = ch_spinbox.value()
+    sensitivity_spinbox_value = ch_sens_spinbox.value()
     scaling_value = fig.mne.scalings[ch_type_test]
     new_scaling_spinbox_value = scaling_spinbox_value * 2
     new_sensitivity_spinbox_value = sensitivity_spinbox_value * 2
     ch_scale_spinbox.setValue(new_scaling_spinbox_value)
     new_scaling_value = fig.mne.scalings[ch_type_test]
     assert scaling_value != new_scaling_value
-    assert round(ch_sens_spinbox.value()) == round(new_sensitivity_spinbox_value)
+    assert round(ch_sens_spinbox.value(), 1) == round(new_sensitivity_spinbox_value, 1)
 
 
 def test_pg_help_dialog(raw_orig, pg_backend):
